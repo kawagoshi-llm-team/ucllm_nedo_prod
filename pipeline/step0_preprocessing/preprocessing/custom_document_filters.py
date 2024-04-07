f668cf30190d56593dc46213aed28a8a01dc8125
tukamilano <milanotukamoto@g.ecc.u-tokyo.ac.jp>
2024-04-07 17:05:05 +0900

    フィルタリングのコードを追加

--- pipeline/step0_preprocessing/preprocessing/custom_document_filters.py
+++ pipeline/step0_preprocessing/preprocessing/custom_document_filters.py
@@ -2,13 +2,15 @@ from hojichar import document_filters, Document
 from fugashi import Tagger
 from ftlangdetect import detect
 
+import unicodedata
+from gensim.models import FastText
+import numpy as np
 
 from os import PathLike
-from typing import Any, Union
+from typing import Any, Union, Optional
 import re
 
 tagger = Tagger('-Owakati')
-
 
 class DiscardAdultContentJa(document_filters.NgWordsFilterJa):
     """
@@ -55,3 +57,120 @@ class SelectJapanese(document_filters.AcceptJapanese):
                 filterd_doc.is_rejected = True
 
         return filterd_doc
+    
+class DiscardWithCharacterRatio():
+    """
+    TokenFilter の実装例です.
+    文字種比率によって文書を排除します.
+    """
+
+    def __init__(self, 
+                 sentence_length_threshold: Optional[int] = 20, #短い方を削除
+                 comma_ratio_threshold: Optional[float] = 0.1, #カンマが多い方を削除
+                 hiragana_ratio_threshold: Optional[float] = 0.2, #ひらがなが少ない方を削除
+                 katakana_ratio_threshold: Optional[float] = None, #カタカナが多い方を削除
+                 kanji_ratio_threshold: Optional[float] = None): #漢字が多い方を削除
+        self.sentence_length_threshold = sentence_length_threshold
+        self.comma_ratio_threshold = comma_ratio_threshold
+        self.hiragana_ratio_threshold = hiragana_ratio_threshold
+        self.katakana_ratio_threshold = katakana_ratio_threshold
+        self.kanji_ratio_threshold = kanji_ratio_threshold
+
+        self.hiragana_chars = set(chr(i) for i in range(12353, 12436))
+        self.katakana_chars = set(chr(i) for i in range(12449, 12534))
+
+    def apply(self, doc: Document) -> Document:
+        normalized_doc = unicodedata.normalize("NFKC", doc.text)
+        normalized_doc = normalized_doc.replace('|', '')
+        normalized_doc = re.sub(r'\【.*?\】', '', normalized_doc)
+        sentences = re.split(r'[\n　.。 ]', normalized_doc)
+
+        filtered_sentences = []
+
+        for sentence in sentences:
+            sentence = sentence.strip()
+            if self.sentence_length_threshold != None:
+                if len(sentence) <= self.sentence_length_threshold:
+                    continue
+
+            if self.comma_ratio_threshold != None:
+                comma_count = sentence.count('、') + sentence.count(',')
+                comma_ratio = comma_count / len(sentence)
+                if comma_ratio > self.comma_ratio_threshold:
+                    continue
+
+            if self.hiragana_ratio_threshold != None:
+                hiragana_count = sum(1 for char in sentence if char in self.hiragana_chars)
+                hiragana_ratio = hiragana_count / len(sentence) 
+                if hiragana_ratio < self.hiragana_ratio_threshold:
+                    continue     
+            
+            if self.katakana_ratio_threshold != None:
+                katakana_count = sum(1 for char in sentence if char in self.katakana_chars)
+                katakana_ratio = katakana_count / len(sentence)
+                if katakana_ratio > self.katakana_ratio_threshold:
+                    continue
+
+            if self.kanji_ratio_threshold != None:
+                # 漢字の正規表現パターン
+                kanji_pattern = re.compile(r'[\u4e00-\u9fff]')
+
+                # 漢字の数をカウント
+                kanji_count = len(kanji_pattern.findall(sentence))
+
+                # 漢字の割合を計算
+                kanji_ratio = kanji_count / len(sentence)
+                if kanji_ratio > self.kanji_ratio_threshold:
+                    continue
+            
+            filtered_sentences.append(sentence)
+
+        doc.text = '。'.join(filtered_sentences)
+        if doc.text != "":
+            doc.text += "。" 
+        else:
+            doc.is_rejected = True
+        return doc
+    
+class DiscardAdultContentWithEmbedding():
+    """
+    TokenFilter の実装例です.
+    DiscardWithCharacterRatioの後に適用することを想定しています。
+    日本語の成人向けコンテンツをEmbeddingを用いて排除します.
+    """
+
+    def __init__(
+        self,
+        adult_threshold: float = 0.25,
+    ) -> None:
+        self.dict_path = "/home/kei.tsukamoto/fastText/cc.ja.300.bin" #自分が入れているfastTextのパスに変えてください
+        self.adult_embedding_path = "/adult_embedding.npy"
+        self.adult_embedding = np.load(self.adult_embedding_path)
+        self.adult_threshold = adult_threshold
+        self.model = FastText.load_fasttext_format(self.dict_path)    
+
+    def calculate_inappropriate_score(self, sentence):
+        text_owakati = tagger.parse(sentence).split()
+        word_scores = []
+        for word in text_owakati:
+            word_embedding = self.model.wv[word]
+            similarity = np.dot(word_embedding, self.adult_embedding)
+            word_scores.append(similarity)
+        
+        return np.nanmean(word_scores)
+
+    def apply(self, doc: Document) -> Document:
+        sentences = re.split('。', doc.text)
+
+        filtered_sentences = []
+
+        for sentence in sentences:
+            if self.calculate_inappropriate_score(sentence) <= self.adult_threshold:
+                filtered_sentences.append(sentence)
+
+        doc.text = '。'.join(filtered_sentences)
+        if doc.text != "":
+            doc.text += "。" 
+        else:
+            doc.is_rejected = True
+        return doc
