8a6f2c7580ab6cc3b15f5f11546baaa7a12e0f7f
tukamilano <107251402+tukamilano@users.noreply.github.com>
2024-04-14 15:38:19 +0900

    Update __main__.py

--- data_management/preprocessing/filtering/__main__.py
+++ data_management/preprocessing/filtering/__main__.py
@@ -3,6 +3,7 @@ import json
 import os
 from hojichar import document_filters, tokenization, Compose, Document
 from preprocessing.filtering import custom_token_filters, custom_tokenization, custom_document_filters
+import pyarrow.parquet as pq
 
 def process_json_line(line: str, cleaner, writer, rejected_writer):
     result = cleaner.apply(Document(line))
@@ -17,11 +18,26 @@ def process_json_line(line: str, cleaner, writer, rejected_writer):
 
 def __readline(input_file: str, start_line=0):
     line_count = 0
-    with open(input_file, "r") as fp:
-        for line in fp:
-            if line_count >= start_line:
-                yield line.strip()
-            line_count += 1
+
+    if input_file.endswith(".gz"):
+        with gzip.open(input_file, "rt") as fp:
+            for line in fp:
+                if line_count >= start_line:
+                    yield line.strip()
+                line_count += 1
+    elif input_file.endswith(".parquet"):
+        table = pq.read_table(input_file)
+        for batch in table.to_batches():
+            for i in range(batch.num_rows):
+                if line_count >= start_line:
+                    yield json.dumps({"text": batch["text"].to_pylist()[i]}, ensure_ascii=False)
+                line_count += 1
+    else:            
+        with open(input_file, "r") as fp:
+            for line in fp:
+                if line_count >= start_line:
+                    yield line.strip()
+                line_count += 1
 
 def get_progress_file_path(output_base, input_file_prefix):
     return os.path.join(output_base, f"{input_file_prefix}_progress.txt")
@@ -45,21 +61,28 @@ def filtering(input_dir: str, output_base: str):
     cleaner = Compose([
         document_filters.JSONLoader(), # テキストをJsonとして解釈し,`key`で指定した要素を文字列としてdoumentに格納.デフォルト`key`は'text'.
         document_filters.DocumentNormalizer(), # 文書の正規化を行う.デフォルトでは全角文字を半角文字に変換する.(NFKC)
-        document_filters.DiscardBBSComments(), # 正規表現 "BBS Patern" に `max_allow_num`(default: 14) 回よりたくさんマッチする文書を破棄。
-        document_filters.DiscardAds(), # 主に広告キーワードを`max_allow_num`より多く含む文書を破棄.
-        document_filters.DiscardDiscriminationContentJa(), # 日本語の差別キーワード(および不適切語)を含む文書を破棄.
+        #document_filters.DiscardBBSComments(), # 正規表現 "BBS Patern" に `max_allow_num`(default: 14) 回よりたくさんマッチする文書を破棄。
+        #document_filters.DiscardAds(), # 主に広告キーワードを`max_allow_num`より多く含む文書を破棄.
+        #document_filters.DiscardViolenceContentJa(), # 日本語の暴力表現キーワードを含む文書を破棄.
+        #document_filters.DiscardDiscriminationContentJa(), # 日本語の差別キーワード(および不適切語)を含む文書を破棄.
         document_filters.DocumentLengthFilter(min_doc_len=10, max_doc_len=100000), # 文書の長さがmin_doc_lenより短い、またはmax_doc_lenより長い文書を破棄.
         custom_document_filters.DiscardAdultContentJa(), # 日本語の成人向けコンテンツを閾値に応じて排除.
-        custom_tokenization.NewLineSentenceTokenizer(), # 改行文字で文章を区切る.
-        custom_token_filters.RemoveOneword(), # 1単語のみ以下のパターンを削除.
-        custom_tokenization.MergeTokens(delimiter="\n"), # 破棄されていないトークンをdelimeterで結合.
+        custom_document_filters.DiscardAds(),
+        custom_document_filters.DiscardViolenceContentJa(),
+        custom_document_filters.DiscardDiscriminationContentJa(),
+        custom_document_filters.DiscardWithCharacterRatio(),
+        #custom_document_filters.DiscardAdultContentWithEmbedding(),
+        #custom_tokenization.NewLineSentenceTokenizer(), # 改行文字で文章を区切る.
+        #custom_token_filters.RemoveOneword(), # 1単語のみ以下のパターンを削除.
+        #custom_tokenization.MergeTokens(delimiter="\n"), # 破棄されていないトークンをdelimeterで結合.
         #custom_tokenization.WakatiTokenizer(), # fugashi を用いて文を分割.
-        custom_token_filters.RemoveDate(), # 日付のみのパターンを削除.
-        tokenization.MergeTokens(), # 破棄されていないトークンを結合.
+        #custom_token_filters.RemoveDate(), # 日付のみのパターンを削除.
+        #tokenization.MergeTokens(), # 破棄されていないトークンを結合.
         #custom_document_filters.SelectJapanese(lookup_size=50), # 日本語以外の文書を排除.
         document_filters.MaskPersonalInformation(), # キュメントに含まれる電話番号・電子メールアドレスを一部マスキング.
         document_filters.JSONDumper(dump_reason=False), # dump_reason=Tureの場合、ドキュメントの破棄事由をJSON形式で付与して出力. JSON形式で出力するため、最後に実行する.（デバッグ用）
     ])
+
 
     for input_file in input_files:
         print("cleaning: " + input_file)
